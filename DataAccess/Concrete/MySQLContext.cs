04b245568d046bf3f1e6f282a60d776f796b215e
lapelyan <lapelyan@outloo.com>
2023-06-04 02:00:05 +0300

    veritabaniDegisim

--- DataAccess/Concrete/MySQLContext.cs
+++ DataAccess/Concrete/MySQLContext.cs
@@ -16,7 +16,18 @@ namespace DataAccess.Concrete
 	public class MySQLContext : DbContext
 	{
 
-		
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseMySql("server=localhost;port=3306;database=Case;user=root;password=aA123456.", new MySqlServerVersion(new Version(8, 0, 26)));
+			}
+		}
 
-	}		
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+		public DbSet<Product> Products { get; set; }
+
+
+	}
 }
